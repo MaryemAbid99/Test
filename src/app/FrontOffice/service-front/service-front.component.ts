46323b4b4478a47a288405495ef0274ee8a05e08
MaryemAbid99 <maryem1999abid@gmail.com>
2025-02-24 21:18:13 +0100

    Initial commit - ajout du projet Angular

---
+++ src/app/FrontOffice/service-front/service-front.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-front',
+  templateUrl: './service-front.component.html',
+  styleUrls: ['./service-front.component.css']
+})
+export class ServiceFrontComponent {
+
+}
